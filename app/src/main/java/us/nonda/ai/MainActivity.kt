482d1f8e82444c28be040f24706340b5fa256d60
zmc <mengchen@nonda.us>
2019-08-27 15:30:55 +0800

    调整初始化mqtt的位置

--- app/src/main/java/us/nonda/ai/MainActivity.kt
+++ app/src/main/java/us/nonda/ai/MainActivity.kt
@@ -3,22 +3,27 @@ package us.nonda.ai
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.TelephonyManager
 import android.util.Log
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import us.nonda.ai.app.receiver.NetStateChangeReceiver
 import us.nonda.ai.app.service.SensorReportService
+import us.nonda.ai.app.ui.AccActivity
 import us.nonda.ai.controler.CarBoxControler
 import us.nonda.commonibrary.MyLog
 import us.nonda.commonibrary.http.NetModule
+import us.nonda.facelibrary.db.DBManager
 
 /**
  * 首页
  */
 class MainActivity : AppCompatActivity() {
-
+    var netStateChangeReceiver: NetStateChangeReceiver? = null
     private val TAG = "MainActivity"
     /**
      * 只会执行一次
@@ -31,11 +36,15 @@ class MainActivity : AppCompatActivity() {
         SensorReportService.startService(this)
         MyLog.d(TAG, "onCreate")
         checkAccStatus(this)
-
+        registReceiver()
         btn_location.setOnClickListener {
             Log.d("VideoRecordActivity", "点击")
-//            CarBoxControler.instance.startCamera(this)
+            CarBoxControler.instance.startCamera(this)
             test()
+        }
+        btn_stop_location.setOnClickListener { view ->
+            val intent=Intent(this,AccActivity::class.java)
+            startActivity(intent)
         }
 
         val carBatteryInfo = CarBoxControler
@@ -44,7 +53,11 @@ class MainActivity : AppCompatActivity() {
         val simNumber = CarBoxControler.instance.getSimNumber(this)
         MyLog.d("设备信息", "电量=$carBatteryInfo   sim卡=$simNumber")
     }
+    private fun registReceiver() {
+        netStateChangeReceiver = NetStateChangeReceiver()
+        registerReceiver(netStateChangeReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
 
+    }
     private fun test() {
         NetModule.instance.provideAPIService()
             .postLicenceSucceed("869455047237132", "111", "123")
@@ -73,8 +86,9 @@ class MainActivity : AppCompatActivity() {
 
         super.onDestroy()
         SensorReportService.stopService(this)
+        DBManager.getInstance().release()
+        unregisterReceiver(netStateChangeReceiver)
 
     }
-
 
 }
--- app/src/main/java/us/nonda/ai/app/ui/AccActivity.kt
+++ app/src/main/java/us/nonda/ai/app/ui/AccActivity.kt
@@ -4,39 +4,35 @@ import android.content.IntentFilter
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import kotlinx.android.synthetic.main.activity_acc.*
 import com.yaoxiaowen.download.DownloadHelper
 import us.nonda.ai.R
-import us.nonda.ai.app.receiver.NetStateChangeReceiver
 import us.nonda.ai.controler.CarBoxControler
 import us.nonda.commonibrary.utils.AppUtils
-import us.nonda.facelibrary.db.DBManager
 import us.nonda.facelibrary.manager.FaceSDKManager
 import us.nonda.mqttlibrary.model.FaceResultBean
 import us.nonda.mqttlibrary.mqtt.MqttManager
 import java.util.ArrayList
+import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.os.postDelayed
+import androidx.core.os.HandlerCompat.postDelayed
+import us.nonda.mqttlibrary.model.GPSBean
+
 
 class AccActivity : AppCompatActivity() {
 
-    var netStateChangeReceiver: NetStateChangeReceiver? = null
 
     private var carBoxControler: CarBoxControler? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acc)
-        registReceiver()
 
         FaceSDKManager.instance.checkLicenceStatus()
-        MqttManager.getInstance().init()
         btn_ota.text = AppUtils.getVersionName(this)
     }
 
-    private fun registReceiver() {
-        netStateChangeReceiver = NetStateChangeReceiver()
-        registerReceiver(netStateChangeReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
-
-    }
 
     fun onOpen(view: View) {
 
@@ -47,12 +43,12 @@ class AccActivity : AppCompatActivity() {
     }
 
     fun onPublish(view: View) {
-        val lists = ArrayList<FaceResultBean>()
-        val faceResultBean = FaceResultBean(1, System.currentTimeMillis())
-//        faceResultBean.face=1
-//        faceResultBean.time=System.currentTimeMillis()
-        lists.add(faceResultBean)
-        MqttManager.getInstance().publishFaceResult(lists)
+        val lists = ArrayList<GPSBean>()
+        for (i in 0..3000) {
+            val gps = GPSBean(120.111,120.121311,3213.12f,3123.213f,121231f,System.currentTimeMillis())
+            lists.add(gps)
+        }
+        MqttManager.getInstance().publishGPS(lists)
     }
 
     fun onOTA(view: View) {
@@ -64,11 +60,24 @@ class AccActivity : AppCompatActivity() {
         mDownloadHelper.addCarBoxTask(this)
     }
 
+    fun onInfinitePuiblish(view: View) {
+        val lists = ArrayList<FaceResultBean>()
+        val faceResultBean = FaceResultBean(1, System.currentTimeMillis())
+        lists.add(faceResultBean)
+        MqttManager.getInstance().publishFaceResult(lists)
+//        handler.postDelayed(runnable, 1000)
+    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        DBManager.getInstance().release()
-        unregisterReceiver(netStateChangeReceiver)
+
+    var handler = Handler()
+    var runnable: Runnable = object : Runnable {
+        override fun run() {
+            val lists = ArrayList<FaceResultBean>()
+            val faceResultBean = FaceResultBean(1, System.currentTimeMillis())
+            lists.add(faceResultBean)
+            MqttManager.getInstance().publishFaceResult(lists)
+            handler.postDelayed(this, 1000)
+        }
     }
 
 }

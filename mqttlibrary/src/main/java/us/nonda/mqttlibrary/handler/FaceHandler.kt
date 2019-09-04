f3037552e3d721b85d19b3793d1b1645ec2b8277
zmc <mengchen@nonda.us>
2019-09-04 20:29:28 +0800

    收到mqtt消息后打印本地日志，修改情绪的采集时间

--- mqttlibrary/src/main/java/us/nonda/mqttlibrary/handler/FaceHandler.kt
+++ mqttlibrary/src/main/java/us/nonda/mqttlibrary/handler/FaceHandler.kt
@@ -1,6 +1,7 @@
 package us.nonda.mqttlibrary.handler
 
 import io.nonda.onedata.proto.contract.CloudDriveMqttMessageCreator
+import us.nonda.commonibrary.MyLog
 import us.nonda.commonibrary.config.CarboxConfigRepostory
 import us.nonda.commonibrary.config.FaceConfig
 import us.nonda.mqttlibrary.mqtt.IMqttMessageHandler
@@ -14,5 +15,6 @@ class FaceHandler : IMqttMessageHandler {
             (cloudDriveMqttFreqData.reportFreq).toLong()
         )
         CarboxConfigRepostory.instance.putFaceConfig(faceConfig)
+        MyLog.d("收到消息","face")
     }
 }
--- mqttlibrary/src/main/java/us/nonda/mqttlibrary/handler/GpsHandler.kt
+++ mqttlibrary/src/main/java/us/nonda/mqttlibrary/handler/GpsHandler.kt
@@ -1,6 +1,7 @@
 package us.nonda.mqttlibrary.handler
 
 import io.nonda.onedata.proto.contract.CloudDriveMqttMessageCreator
+import us.nonda.commonibrary.MyLog
 import us.nonda.commonibrary.config.CarboxConfigRepostory
 import us.nonda.commonibrary.config.GpsConfig
 import us.nonda.mqttlibrary.mqtt.IMqttMessageHandler
@@ -14,5 +15,6 @@ class GpsHandler : IMqttMessageHandler {
             (cloudDriveMqttFreqData.reportFreq).toLong()
         )
         CarboxConfigRepostory.instance.putGpsConfig(gpsConfig)
+        MyLog.d("收到消息","gpsConfig")
     }
 }
